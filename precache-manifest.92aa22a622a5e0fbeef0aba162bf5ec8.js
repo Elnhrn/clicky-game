ed3d19f3b65ecde31ecf816a84ffe7b738ee436f
elaine <user@host.com>
2018-10-20 13:49:24 -0700

    Updates

---
+++ precache-manifest.92aa22a622a5e0fbeef0aba162bf5ec8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb2d2b8656e869531cd9",
+    "url": "/static/js/main.eb2d2b86.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "eb2d2b8656e869531cd9",
+    "url": "/static/css/main.055eea7d.chunk.css"
+  },
+  {
+    "revision": "0b1a1ca3ef927e4b38ffeb62626e59eb",
+    "url": "/index.html"
+  }
+];
